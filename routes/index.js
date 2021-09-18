411e29852ccf40e20f31601d57005ec2788d024e
Alan Hou <alanhou911222@gmail.com>
2021-09-18 16:08:34 -0400

    setup enterData page

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/enterData', function(req, res, next) {
+  res.render('enterData', { title: 'Express' });
+});
+
+
 module.exports = router;
