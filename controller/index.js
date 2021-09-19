82d7be1ca04fb164ec8f19b4453c92774826a332
Exitialium <henrywu0424@gmail.com>
2021-09-19 08:47:33 -0400

    Controllers Login change navbar

---
+++ controller/index.js
@@ -0,0 +1,135 @@
+var isAuthenticated = false;
+var image = null;
+var displayName = null;
+
+exports.index = function(req, res, next) {
+  if(req.isAuthenticated()){
+    displayName = req.user.displayName;
+    isAuthenticated = true;
+    image = req.user.image;
+  }else{
+    displayName = null;
+    isAuthenticated = false;
+    image = null;
+  }
+  res.render('index', {
+    loggedin: isAuthenticated,
+    name: displayName,
+    image: image,
+  });
+}
+
+exports.profile = function(req, res, next) {
+    if(req.isAuthenticated()){
+      displayName = req.user.displayName;
+      isAuthenticated = true;
+      image = req.user.image;
+    }else{
+      displayName = null;
+      isAuthenticated = false;
+      image = null;
+    }
+    res.render('userInfo', {
+      loggedin: isAuthenticated,
+      name: displayName,
+      image: image,
+    });
+}
+
+exports.post = function(req, res, next) {
+    if(req.isAuthenticated()){
+      displayName = req.user.displayName;
+      isAuthenticated = true;
+      image = req.user.image;
+    }else{
+      displayName = null;
+      isAuthenticated = false;
+      image = null;
+    }
+    res.render('post/viewpost', {
+      loggedin: isAuthenticated,
+      name: displayName,
+      image: image,
+    });
+}
+
+exports.enterData = function(req, res, next) {
+    if(req.isAuthenticated()){
+      displayName = req.user.displayName;
+      isAuthenticated = true;
+      image = req.user.image;
+    }else{
+      displayName = null;
+      isAuthenticated = false;
+      image = null;
+    }
+    res.render('enterData', {
+      loggedin: isAuthenticated,
+      name: displayName,
+      image: image,
+    });
+}
+exports.editUser = function(req, res, next) {
+    if(req.isAuthenticated()){
+      displayName = req.user.displayName;
+      isAuthenticated = true;
+      image = req.user.image;
+    }else{
+      displayName = null;
+      isAuthenticated = false;
+      image = null;
+    }
+    res.render('editUser', {
+      loggedin: isAuthenticated,
+      name: displayName,
+      image: image,
+    });
+}
+exports.orders = function(req, res, next) {
+    if(req.isAuthenticated()){
+      displayName = req.user.displayName;
+      isAuthenticated = true;
+      image = req.user.image;
+    }else{
+      displayName = null;
+      isAuthenticated = false;
+      image = null;
+    }
+    res.render('orders', {
+      loggedin: isAuthenticated,
+      name: displayName,
+      image: image,
+    });
+}
+exports.dashboard = function(req, res, next) {
+    if(req.isAuthenticated()){
+      displayName = req.user.displayName;
+      isAuthenticated = true;
+      image = req.user.image;
+    }else{
+      displayName = null;
+      isAuthenticated = false;
+      image = null;
+    }
+    res.render('buyerDashboard', {
+      loggedin: isAuthenticated,
+      name: displayName,
+      image: image,
+    });
+}
+exports.about = function(req, res, next) {
+    if(req.isAuthenticated()){
+      displayName = req.user.displayName;
+      isAuthenticated = true;
+      image = req.user.image;
+    }else{
+      displayName = null;
+      isAuthenticated = false;
+      image = null;
+    }
+    res.render('about', {
+      loggedin: isAuthenticated,
+      name: displayName,
+      image: image,
+    });
+}
