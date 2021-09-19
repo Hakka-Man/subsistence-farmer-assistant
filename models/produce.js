81ce4ad32d1f815582ccdb4ed3f4cba5a1da6a52
chi-wei-lien <lien2@purdue.edu>
2021-09-19 08:11:14 -0400

    change model structure

---
+++ models/produce.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose')
+//CockroachDB
+const Sequelize = require("sequelize-cockroachdb");
+
+// For secure connection to CockroachDB
+const fs = require('fs');
+
+// Connect to CockroachDB through Sequelize
+var sequelize = new Sequelize({
+  dialect: "postgres",
+  username: "alan",
+  password: "haaxEmGrSBlZII6S",
+  host: "free-tier.gcp-us-central1.cockroachlabs.cloud",
+  port: 26257,
+  database: "subsistance-farmer-3652.bank",
+  dialectOptions: {
+    ssl: {
+
+      //For secure connection:
+      ca: fs.readFileSync('certs/cc-ca.crt')
+              .toString()
+    },
+  },
+  logging: false,
+});
+
+
+const Produce = sequelize.define("produce", {
+  produceName: {
+      type: Sequelize.TEXT,
+  },
+  price: {
+      type: Sequelize.INTEGER,
+  },
+  slogan: {
+      type: Sequelize.TEXT,
+  },
+  discription: {
+    type: Sequelize.TEXT,
+  },
+  recipes: {
+    type: Sequelize.TEXT,
+  },
+
+});
+
+module.exports = Produce
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var Produce = require('../models/produce.js')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -33,6 +34,68 @@ router.get('/dashboard', function(req, res, next) {
 router.get('/about', function(req, res, next) {
   res.render('about');
 });
+
+// router.get('/test', (req, res) => {
+//     //Get our data from CockroachDB
+//     Produce.sync({
+//          force:false,
+//     })
+//     .then(function() {
+//        return Produce.findAll();
+//     })
+//     .then(function (produce) {
+//         //Render output from CockroachDB using our PUG template
+//         res.render('test', { produce : produce });
+//     })
+// });
+
+router.get('/test', async function (req, res) {
+    Produce.sync({
+         force:false,
+    })
+    var produce = await Produce.findAll();
+    console.log(produce);
+    res.render('test', { produce : produce});
+})
+
+
+router.post('/submit', function (req, res) {
+
+  //Get our values submitted from the form
+  var fromName = req.body.produceName;
+  var fromPrice = req.body.price;
+  var fromslogan = req.body.slogan;
+  var fromDiscription = req.body.discription;
+  var fromRecipes = req.body.recipes;
+
+  //Add our POST data to CockroachDB via Sequelize
+  Produce.sync({
+      force: false,
+  })
+      .then(function () {
+      // Insert new data into Produce table
+      return Produce.bulkCreate([
+          {
+          produceName: fromName,
+          price: fromPrice,
+          slogan: fromslogan,
+          discription: fromDiscription,
+          recipes: fromRecipes,
+          },
+      ]);
+      })
+
+      //Error handling for database errors
+      .catch(function (err) {
+      console.error("error: " + err.message);
+      });
+
+      //Tell them it was a success
+      // res.send('Submitted Successfully!<br /> Name:  ' + fromName + '<br />Price:  ' + fromPrice);
+      console.log('Submitted Successfully!<br /> Name:  ' + fromName + '<br />Price:  ' + fromPrice)
+      res.redirect("test");
+});
+
 
 
 
