769f4e393551088b1e161bd279dc5530550950e5
chi-wei-lien <lien2@purdue.edu>
2021-09-19 10:52:48 -0400

    remove db

--- config/passport.js
+++ config/passport.js
@@ -1,7 +1,6 @@
 const GoogleStrategy = require('passport-google-oauth20').Strategy
 const mongoose = require('mongoose')
 const User = require('../models/User')
-const CockroachUser = require('../models/CockroachUser')
 
 module.exports = function(passport){
     passport.use(new GoogleStrategy({
